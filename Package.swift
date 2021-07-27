06c0357f85557c862e975330ad4e3ed038b1356b
Filip Bajaník <filip.bajanik@gmail.com>
2021-07-27 21:31:00 +0200

    feat: remove combine-schedulers and add support for iOS 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-composable-architecture",
   platforms: [
-    .iOS(.v13),
+    .iOS(.v12),
     .macOS(.v10_15),
     .tvOS(.v13),
     .watchOS(.v6),
@@ -17,7 +17,6 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.1.3"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
     .package(url: "https://github.com/mxcl/PromiseKit", from: "7.0.0-rc2")
@@ -27,7 +26,6 @@ let package = Package(
       name: "ComposableArchitecture",
       dependencies: [
         "CasePaths",
-        "CombineSchedulers",
         "XCTestDynamicOverlay",
         "PromiseKit"
       ]
